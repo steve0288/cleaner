aec8c4fcd2fd53a28999eab3e25841e7813ae657
Steve <watto_70@hotmail.com>
2023-06-26 22:11:21 +1000

    Proj Start

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,15 @@ module.exports = {
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-empty-function': 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'linebreak-style': 'off'
   },
   overrides: [
     {
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="navbar-header">
+    <img class="logo" src="img/icons/hole-crew-logo.png" alt="Hole Crew Logo" />
+    <img alt="Vue logo" src="assets/logo.png"/>
+    <nav>
+      <a href="#service">Service</a>
+      <a href="#prices">Prices</a>
+      <a href="#about-us">About Us</a>
+      <a href="#contact-us">Contact Us</a>
+      <!--<router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>-->
+    </nav>
+    <router-view />
+    <div class="contact-us">
+    </div>
+  </div>
+
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: DM Sans, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,5 +37,27 @@ nav {
       color: #42b983;
     }
   }
+}
+
+@font-face {
+  font-family: "DM Sans";
+  font-style: normal;
+  src: url("assets/fonts/DMSans-Regular.woff2") format("woff2"),
+    url("assets/fonts/DMSans-Regular.ttf") format("ttf");
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "DM Sans";
+  src: url("assets/fonts/DMSans-Medium.woff2") format("woff2"),
+    url("assets/fonts/DMSans-Regular.ttf") format("ttf");
+  font-weight: 500;
+}
+
+@font-face {
+  font-family: "DM Sans";
+  src: url("assets/fonts/DMSans-Bold.woff2") format("woff2"),
+    url("assets/fonts/DMSans-Regular.ttf") format("ttf");
+  font-weight: 700;
 }
 </style>

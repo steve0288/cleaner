598b314f54483ace71d7291b3bc8e99c31d6da46
Steve <watto_70@hotmail.com>
2023-10-09 09:02:35 +1100

    Fixed page references

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PricesView from '../views/PricesView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,15 +9,20 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component: HomeView
   },
-  {
+  /* {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+    component: () => import(/* webpackChunkName: "about" / '../views/AboutView.vue')
+  }, */
   {
     path: '/prices',
     name: 'prices',
     component: PricesView
+  },
+  {
+    path: '/about-us',
+    name: 'about',
+    component: AboutView
   }
 ]
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about section section-width">
+    Welcome to the Hole Crew, your trusted partner in the world of professional cleaning services. With over a decade of experience, we have established ourselves as a leading name in the cleaning industry, serving countless satisfied customers throughout Sydney.
+  </div>
+  <div class="section section-width">
+    <h1>Our Journey</h1>
+    <p>Since our inception in 2013, we have been on a mission to transform the way you experience cleanliness. Over the years, we've honed our skills, built strong relationships, and adapted to the ever-evolving demands of our clients. Our journey has been marked by unwavering commitment, dedication, and a genuine passion for making spaces cleaner and healthier.
+    </p>
+  </div>
+  <div class="section section-width">
+    <h1>Why Choose Us?</h1>
+    <p><span class="bold">Experience:</span> With more than 10 years in the business, we bring a wealth of experience to every job we undertake. Our seasoned team knows the ins and outs of cleaning, ensuring exceptional results every time.</p>
+    <p><span class="bold">Extensive Contacts:</span> Our vast network of industry contacts allows us to stay at the forefront of cleaning innovations. We have access to the best tools, techniques, and resources, ensuring your space receives the highest standard of care.</p>
+    <p><span class="bold">Friendly Service:</span> At the Hole Crew, we believe in the power of a warm smile and a friendly approach. We treat your space as if it were our own, and our team is always ready to go the extra mile to exceed your expectations.</p>
+    <div><h2>Our Services</h2>From residential cleaning to commercial janitorial services, we offer a comprehensive range of cleaning solutions tailored to your unique needs. Whether it's a one-time deep clean, regular maintenance, or a specialized service, we've got you covered.</div>
+    <div><h2>Our Commitment</h2>We are committed to delivering excellence in every job we undertake. Our dedication to quality, reliability, and customer satisfaction is the driving force behind everything we do. When you choose the Hole Crew, you're choosing a partner who cares about your space as much as you do.</div>
+    <div><h2>Join Our Family</h2>We consider our clients to be a part of our extended family. Join us in the pursuit of cleanliness, health, and well-being. Experience the difference that a decade of expertise, extensive contacts, and friendly service can make in your life.</div>
+    <p>Thank you for considering the Hole Crew for your cleanisng needs. We look forward to serving you and exceeding your expectations. Contact us today to schedule your next cleaning appointment.</p>
   </div>
 </template>
 <script lang="ts">
